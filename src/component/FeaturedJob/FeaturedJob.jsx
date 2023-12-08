4a915d4e5a7dc9e2f03c7a5cfad7ce13244687e4
naimurcse <2015naim@gmail.com>
2023-12-08 21:12:11 +0600

    Style of FeaturedJob is done

--- src/component/FeaturedJob/FeaturedJob.jsx
+++ src/component/FeaturedJob/FeaturedJob.jsx
@@ -8,27 +8,30 @@ const FeaturedJob = ({ featuredJob }) => {
    return (
       <div className="featured-job">
          <img src={img} alt="" />
-         <h2>{designation}</h2>
-         <p>{companyName}</p>
-         {isRemote ? (
-            <button className="btn btn-outline btn-primary-outline">Remote</button>
-         ) : (
-            <button className="btn btn-outline btn-primary-outline">Onsite</button>
-         )}
-         {isFullTime ? (
-            <button className="btn btn-outline btn-primary-outline">Full Time</button>
-         ) : (
-            <button className="btn btn-outline btn-primary-outline">Part Time</button>
-         )}
-         <div className="featured-job__info">
-            <p>
-               <FontAwesomeIcon icon={faLocationDot} />
-               <span>{location}</span>
-            </p>
-            <p>
-               <FontAwesomeIcon icon={faSackDollar} />
-               <span>Salary: {salary}</span>
-            </p>
+
+         <div>
+            <h2>{designation}</h2>
+            <p className="company-name">{companyName}</p>
+            {isRemote ? (
+               <button className="btn btn-outline btn-primary-outline mr-20">Remote</button>
+            ) : (
+               <button className="btn btn-outline btn-primary-outline mr-20">Onsite</button>
+            )}
+            {isFullTime ? (
+               <button className="btn btn-outline btn-primary-outline">Full Time</button>
+            ) : (
+               <button className="btn btn-outline btn-primary-outline">Part Time</button>
+            )}
+            <div className="featured-job__info">
+               <p>
+                  <FontAwesomeIcon icon={faLocationDot} />
+                  <span>{location}</span>
+               </p>
+               <p>
+                  <FontAwesomeIcon icon={faSackDollar} />
+                  <span>Salary: {salary}</span>
+               </p>
+            </div>
          </div>
 
          <button className="btn btn-medium btn-primary">View Details</button>
